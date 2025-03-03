3ec4edbcdc8599d630d95b80f5dea8b9869b92cf
salarmotevalli <salar.mo.ro@gmail.com>
2025-03-03 23:30:00 +0330

    done

--- etc/database/tables/asnwers.sql
+++ etc/database/tables/asnwers.sql
@@ -1,6 +1,7 @@
 
 CREATE TABLE `answers` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `bot_id` VARCHAR NOT NULL, 
     `stored_at` DATE NOT NULL,
     `content_length` INTEGER NOT NULL
 );
--- etc/database/tables/responses.sql
+++ etc/database/tables/responses.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `responses` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `bot_id` VARCHAR NOT NULL,
     `stored_at` DATE NOT NULL,
     `content_length` INTEGER NOT NULL
 );
